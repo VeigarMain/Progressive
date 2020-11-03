ba433c5c23da6d7f02bafe38a39eed45db8c337b
Jake Battaglia <jbaked12@gmail.com>
2020-11-03 15:11:10 -0500

    finished progressive budget app, need to add styling

---
+++ routes/api.js
@@ -0,0 +1,34 @@
+const router = require("express").Router();
+const Transaction = require("../models/transactions.js");
+
+router.post("/api/transactions", ({body}, res) => {
+  Transaction.create(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+router.post("/api/transactions/bulk", ({body}, res) => {
+  Transaction.insertMany(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+router.get("/api/transactions", (req, res) => {
+  Transaction.find({}).sort({date: -1})
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+module.exports = router;
